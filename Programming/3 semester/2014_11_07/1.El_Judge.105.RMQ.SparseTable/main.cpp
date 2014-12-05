d9042ebed1205f31fec8dc76b11b81a376060afe
volodden <volodin@phystech.edu>
2014-12-05 03:49:16 +0300

    Sparse Table.

---
+++ Programming/3 semester/2014_11_07/1.El_Judge.105.RMQ.SparseTable/main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+
+class RMQSparseTable
+{
+private:
+
+    std::vector < std::vector < float > > ST;
+    std::vector < short > logarithm;
+    std::vector < int > powerOfTwo;
+
+public:
+
+    RMQSparseTable(int N)
+    {
+        logarithm.resize(N + 1);
+        char r = 0;
+        int q = 1;
+        powerOfTwo.push_back(1);
+        powerOfTwo.push_back(2);
+        for (int i = 1; i < N + 1; ++i)
+        {
+            if (i == powerOfTwo[powerOfTwo.size() - 1])
+            {
+                ++r;
+                powerOfTwo.push_back(i * 2);
+            }
+            logarithm[i] = r;
+        }
+        ST.resize(logarithm[N] + 1);
+        ST[0].resize(N);
+        for (int i = 0; i < N; ++i)
+        {
+            float k;
+            std::cin >> k;
+            ST[0][i] = k;
+        }
+        int k = logarithm[ST[0].size()];
+        for (int i = 1; i <= k; ++i)
+        {
+            ST[i].resize(ST[i-1].size() - powerOfTwo[i - 1]);
+            for (int j = 0; j < ST[i].size(); ++j)
+            {
+                ST[i][j] = minimum(ST[i - 1][j], ST[i - 1][j + powerOfTwo[i - 1]]);
+            }
+        }
+    }
+
+    float min(int a, int b)
+    {
+        if (a + 1 != b)
+        {
+            return minimum(ST[logarithm[b - a]][a], ST[logarithm[b - a]][b - powerOfTwo[logarithm[b - a]]]);
+        }
+        else
+        {
+            return ST[0][a];
+        }
+    }
+
+private:
+
+    float minimum(float A, float B)
+    {
+        return (A < B) ? A : B;
+    }
+};
+
+int main()
+{
+    int N = 0;
+    std::cin >> N;
+    RMQSparseTable rmq = RMQSparseTable(N);
+    std::cin >> N;
+    int a, b;
+    for (int i = 0; i < N; ++i)
+    {
+        std::cin >> a >> b;
+        std::cout << rmq.min(a, b) << '\n';
+    }
+    return 0;
+}
