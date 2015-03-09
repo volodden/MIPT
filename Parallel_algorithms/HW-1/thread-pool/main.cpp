160fa07cc4b284bfcf47c60c4875b7e2f3dfc580
volodden <volodin@phystech.edu>
2015-03-09 20:34:27 +0300

    Add task 1.

--- Parallel_algorithms/HW-1/thread-pool/main.cpp
+++ Parallel_algorithms/HW-1/thread-pool/main.cpp
@@ -4,9 +4,10 @@
 #include "ReturnedValue.hpp"
 #include "ThreadPool.hpp"
 
-void printNumber(int i = 0)
+int printNumber(int i = 23)
 {
 	std::cout << i << '\n';
+	return i;
 }
 
 int main()
@@ -15,15 +16,15 @@ int main()
 	
 	/*
 	// store a free function
-	std::function<void(int)> f_free = printNumber;
+	std::function<int(int)> f_free = printNumber;
 	ReturnedValue<int> a = tp.submit(f_free);
 
 	// store a lambda
-	std::function<void()> f_labmda = []() { printNumber(42); };
+	std::function<int()> f_labmda = []() { printNumber(42); };
 	ReturnedValue<int> b = tp.submit(f_labmda);
 
 	// store the result of a call to std::bind
-	std::function<void()> f_bind = std::bind(printNumber, 31337);
+	std::function<int()> f_bind = std::bind(printNumber, 31337);
 	ReturnedValue<int> c = tp.submit(f_bind);
 
 	std::cout << "a = " << a.get() << "\nb = " << b.get() << "\nc = " << c.get() << '\n';
