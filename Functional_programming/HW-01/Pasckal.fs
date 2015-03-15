b1caae0da21ea64692103f201010f3bbbc0806ad
volodden <volodin@phystech.edu>
2015-03-15 17:20:05 +0300

    FP_Pascal

---
+++ Functional_programming/HW-01/Pasckal.fs
@@ -0,0 +1,15 @@
+﻿let rec pascal c r = 
+    if ((c < 0) || (r < 0) || (c > r))
+    then 0
+    else if ((c = 0) || (c = r))
+         then 1
+         else (pascal (c-1) (r-1)) + (pascal (c) (r-1))
+
+let printIt n = 
+  "[" +
+  ([for x in 0..n do for y in 0..x do yield pascal y x] 
+    |> List.map (fun x -> x.ToString())
+    |> List.reduce (fun x y -> x + "," + y) )
+  + "]"
+
+let answer = printIt 20
