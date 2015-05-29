b8b1bfc00273c79495e4e656a8b7738285879e63
volodden <volodin@phystech.edu>
2015-05-30 02:55:14 +0300

    Update lab2.

--- Functional_programming/HW-05/lab2.fs
+++ Functional_programming/HW-05/lab2.fs
@@ -63,18 +63,3 @@ let Lab_02() =
         |> List.filter (fun (x, y) -> getLanguage x)
         |> List.map (fun (x, y) -> y)
         |> List.toArray
-
-// почтовый адрес
-let email = ""
- 
-let main () =
-  let values = new NameValueCollection()
-  values.Add("email", email)
-  values.Add("result", Lab_02().ToString())
-  values.Add("content", File.ReadAllText(__SOURCE_DIRECTORY__ + @"/" + __SOURCE_FILE__))
- 
-  let client = new WebClient()
-  let response = client.UploadValues(new Uri("http://mipt.eu01.aws.af.cm/lab2"), values)
-  let responseString = Text.Encoding.Default.GetString(response)
- 
-  printf "%A\n" responseString
